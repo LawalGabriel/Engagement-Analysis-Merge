c6507da2172255d122445900178f520740cc9797
Mohammed Akanji <akanjimm@gmail.com>
2023-03-30 13:24:26 +0100

    (feat): allow quering a user's analytics using email

--- tabs/src/components/helperComponents/drowdown.jsx
+++ tabs/src/components/helperComponents/drowdown.jsx
@@ -4,21 +4,9 @@ import { toasterErrorMessage } from "../utils/errorHandlingUtils";
 import { useData } from "@microsoft/teamsfx-react";
 import { Toaster } from "react-hot-toast";
 
-const inputItems = [
-    {
-        header: 'Bruce Wayne',
-        content: 'Software Engineer',
-    },
-    {
-        header: 'Natasha Romanoff',
-        content: 'UX Designer 2',
-    }
-]
-
-
-export function TestDropdown(props) {
-    let { apiClient, triggerConsent } = { ...props };
-    let [dropdownData, setDropDownData] = useState();
+export default function TestDropdown(props) {
+    const { apiClient, triggerConsent } = { ...props };
+    const [dropdownData, setDropDownData] = useState();
     const [apiData, setApiData] = useState(undefined);
     const [isClicked, setIsClicked] = useState(false);
 
@@ -29,6 +17,7 @@ export function TestDropdown(props) {
             let processedData = response.data.map((user) => {
                 return { header: user.userPrincipalName, content: user.displayName }
             });
+            processedData = [{ header: "All", content: "All Users" }, ...processedData]
             setDropDownData(processedData)
         } catch (error) {
             let errorMessage = error.response.data.error;
@@ -42,16 +31,13 @@ export function TestDropdown(props) {
 
 
     const handleChange = async (event) => {
-        console.log("........... This is working")
-console.log(event);
         setIsClicked(true);
         setApiData();
         try {
-            const response = await apiClient.get(`analytics?upn=${event.value.header}`);
+            const response = await apiClient.get(`analytics?userUpn=${event.value.header}`);
             setIsClicked(false);
             setApiData(response.data);
         } catch (error) {
-            console.log(error)
             setIsClicked(false);
             let errorMessage = error.response.data.error;
             if (errorMessage.includes("invalid_grant")) {
@@ -77,7 +63,7 @@ console.log(event);
                 placeholder="Start typing a name"
                 getA11ySelectionMessage={getA11ySelectionMessage}
                 noResultsMessage="We couldn't find any matches."
-                onChange={async (_,event) => await handleChange(event)}
+                onChange={async (_, event) => await handleChange(event)}
             />}
             {isClicked && !apiData && (
                 <pre className="fixed">
